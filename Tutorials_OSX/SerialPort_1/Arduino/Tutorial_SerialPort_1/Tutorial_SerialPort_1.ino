694fbcea9b308a03e5139564bdf047ca1519bb3e
Nicolas Gachadoit <Nicolas Gachadoit>
2017-01-23 15:01:30 +0100

    Various improvements

--- Tutorials_OSX/SerialPort_1/Arduino/Tutorial_SerialPort_1/Tutorial_SerialPort_1.ino
+++ Tutorials_OSX/SerialPort_1/Arduino/Tutorial_SerialPort_1/Tutorial_SerialPort_1.ino
@@ -1,18 +1,24 @@
 /*
- Arduino example for MyViz tutorial 1 on Serial Port communication
+ Arduino example for MyViz tutorial 2 on Serial Port communication
 
  The LED is controlled by a MyViz dashboard.
  A value controlled by a slider in the MyViz dashboard is sent back to the dashboard.
- The values on A0 and A1 are displayed on this dashboard.
+ The values on A0 and A1 are displayed on this dashboard, after multiplication by a factor
+ sent from a select control on the dashboard.
 
- Created 13 July 2016
+ Created 6 September 2016
  by Nicolas Gachadoit
 
  This example code is in the public domain.
  */
 
-// Pins définition:
+// Pins definitions:
 const int LED = 13;
+
+// Variables definitions
+int onoff = 0; // On/off switch value (0 or 1)
+float val = 0.0; // Value controlled by a slider (between 0 and 10)
+int mult = 1; // Multiplication factor, controlled by a select widget (between 1 and 3)
 
 void setup() {
   // Initialize serial port:
@@ -28,28 +34,35 @@ void loop() {
   while (Serial.available() > 0) {
 
     // Look for the next valid integer in the incoming serial stream:
-    int onoff = Serial.parseInt(); // On/off switch value (0 or 1)
+    onoff = Serial.parseInt();
     // do it again:
-    float val = Serial.parseFloat(); // Value controlled by a slider (between 0 and 10)
+    val = Serial.parseFloat();
+    // do it again:
+    mult = Serial.parseInt();
 
     // Look for the newline. That's the end of your
     // sentence:
     if (Serial.read() == '\r') {
-      // Switch the LED
-      digitalWrite(LED, onoff);
-      
-      // Read A0 and A1
-      int a0 = analogRead(0);
-      int a1 = analogRead(1);
-      
-      // Print these values separated with comma
-      Serial.print(val);
-      Serial.print(",");
-      Serial.print(a0);
-      Serial.print(",");
-      Serial.println(a1);
+      break;
     }
+    
   }
+  // Switch the LED
+  digitalWrite(LED, onoff);
+  
+  // Read A0 and A1 and multiply them
+  int multA0 = mult * analogRead(0);
+  int multA1 = mult * analogRead(1);
+  
+  // Print these values separated with comma
+  Serial.print(val);
+  Serial.print(",");
+  Serial.print(multA0);
+  Serial.print(",");
+  Serial.println(multA1);
+
+  // Slight delay to avoid too much data
+  delay(200);
 
 }
 
